d020b8b2426d8bd7567dd52c1c5181777a13a596
Kunal Sharma <kunalsharmajpr557@gmail.com>
2026-09-01 16:55:38 +0530

    Initial commit - Vatsalya Public School website

---
+++ script.js
@@ -0,0 +1,4 @@
+const io = new IntersectionObserver((entries)=>{
+    entries.forEach(e=>{ if(e.isIntersecting){ e.target.classList.add('show'); } });
+  }, {threshold:0.12});
+  document.querySelectorAll('.fade-in').forEach(el=>io.observe(el));
